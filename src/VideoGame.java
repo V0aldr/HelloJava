64786c625cddccacdfdd5ce533a90017eabcee4e
voaldr <vorobiovss@gmail.com>
2024-06-12 10:59:44 +0300

    примеры синтаксиса

---
+++ src/VideoGame.java
@@ -0,0 +1,26 @@
+public class VideoGame {
+    String color; // цвет
+    int price; // цена
+
+    void start () {
+    }
+    void stop () {
+    }
+    void saveScore (String playersName, int score) {
+    }
+}
+
+
+class GameBoyAdvance{
+    int screenWidth;
+    String cartridgeType;
+    int myScore=10;
+
+    void prnt(){
+        System.out.println(myScore);
+    }
+    void startGame (){}
+    void spotGame () {}
+
+}
+

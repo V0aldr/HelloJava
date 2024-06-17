440c62a4f82b142b7a803abe27c6b60209fe5a3f
voaldr <vorobiovss@gmail.com>
2024-06-17 19:18:40 +0300

    Nested for // stars patterns

--- src/stepic/NestedTriangleStar.java
+++ src/stepic/NestedTriangleStar.java
@@ -1,4 +1,33 @@
 package stepic;
 
+import java.util.Scanner;
+
 public class NestedTriangleStar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int height = scan.nextInt();
+
+        if (height <= 0) {
+            System.out.println("ERROR");
+            return;
+        }
+
+        for (int i = height; i >= 0; i--) {
+            for (int j = height; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
+
+
+
+
+

3b3d20d9bda16c44b72ebda3053c009739edc21b
voaldr <vorobiovss@gmail.com>
2024-06-20 12:37:48 +0300

    Random & array

---
+++ src/stepic/array/ArrMinMax.java
@@ -0,0 +1,38 @@
+package stepic.array;
+
+import javax.sound.midi.MidiFileFormat;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrMinMax {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] list = new int[scan.nextInt()];
+        int seed = scan.nextInt();
+        Random rand = new Random(seed);
+
+        for (int i = 0; i < list.length; i++) {
+            list[i] = rand.nextInt(-5, 16);
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+
+        int maxIndex = 0, minIndex = 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] >= list[maxIndex]) {
+                maxIndex = i;
+            } else if (list[i] < 0){
+                minIndex = i;
+            }
+        }
+
+        int temp = list[maxIndex];
+        list[maxIndex] = list[minIndex];
+        list[minIndex] = temp;
+
+        for (int el : list) System.out.print(el + " ");
+    }
+}
+
+//10 77 	7 -5 -1 5 -3 8 2 -1 -3 -3
+//          7 -5 -1 5 -3 -3 2 -1 -3 8

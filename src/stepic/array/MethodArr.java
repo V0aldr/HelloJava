d359f7efd97a97480c156b8623250c67ec36483e
voaldr <vorobiovss@gmail.com>
2024-06-21 10:05:45 +0300

    Methods

--- src/stepic/array/MethodArr.java
+++ src/stepic/array/MethodArr.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MethodArr {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int[] ar = new int[scan.nextInt()];
         long seed = scan.nextLong();
@@ -15,13 +15,25 @@ public class MethodArr {
 
     }
 
-    public static void init(int[] array, long seed){
+    public static void init(int[] array, long seed) {
         Random rand = new Random(seed);
-        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(-3, 6);
+        }
     }
 
-    public static void print(){}
+    public static void print(int[] array) {
+        for (int numbers : array) {
+            System.out.print(numbers + " ");
+        }
+        System.out.println();
+    }
 
-    public static void findMax(){}
-
+    public static int findMax(int[] array) {
+        int imax = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > array[imax]) imax = i;
+        }
+        return imax;
+    }
 }
